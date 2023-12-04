30b678f3fdf3297789ca45f9d59535e499201547
Matthew_Hill <hillmatt58@gmail.com>
2023-12-04 11:14:12 -0700

    inital files

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const apiRoutes = ('./routes/routes')(app)
