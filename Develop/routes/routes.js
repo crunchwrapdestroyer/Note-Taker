f46691d8b7a574cdfee56b2444967c0b7c12313f
Matthew_Hill <hillmatt58@gmail.com>
2023-12-10 13:15:46 -0700

    Created delete route but not yet working

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -2,21 +2,29 @@ const { error } = require('console')
 const fs = require('fs')
 const path = require('path')
 const router = require('express').Router()
+const { v4: uuidv4 } = require('uuid')
 
 //Set up /api/notes GET route
 router.get('/api/notes', (req, res) => {
-    // const noteId = req.params.id
+
     fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err,data) => {
         if (err) throw err
         console.error(err)
         const notes = JSON.parse(data)
-        // const foundNote = notes.find(note => note.id === parseInt(noteId))
-        res.json(notes)
+
+        const newUuID = uuidv4
+        const notesWithUuid = notes.map(note => ({
+            ...note,
+            id: uuidv4(),
+        }))
+        res.json(notesWithUuid)
     })
 })
-    
+
+
+
 //Set up * GET route for index.html
-        
+
 //Set up /api/notes POST route
 router.post('/api/notes', (req, res) => {
     const newNote = req.body
@@ -40,6 +48,35 @@ router.post('/api/notes', (req, res) => {
         console.log('Added  new note: ' + newNote.title)
     })
 
+router.delete('/api/notes/:id', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err,data) => {
+        if (err) {
+            console.error(err)
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        const parsedData = JSON.parse(data)
+        const noteIndex = parsedData.findIndex(note => note.id === req.params.id)
+
+        if (noteIndex === -1) {
+            res.status(404).json({ error: 'Note not found' })
+            return
+        }
+        // Remove note from the array
+        parsedData.splice(noteIndex, 1) 
+        // Write updated data back to the file
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(parsedData), (err) => {
+            if (err) {
+                console.error(err)
+                res.status(500).json({ error: 'Internal Server Error' })
+                return
+            }
+            res.json ({ success: true })
+            console.log('Deleted note with ID: ' + req.params.id)
+        })
+    })
+})    
+
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
@@ -47,7 +84,7 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 router.get('/api/notes:id', (req, res) => {
-    
+    res.json(notes[req.params.id])
 })
 module.exports = router
 
